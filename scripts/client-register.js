e72dbcfbd589b903b0b0595e20ae8893b36d3748
Vinicius Nascimento] <viniciusmeloreisdonascimento@gmail.com>
2025-05-27 16:19:38 -0300

    eye-icon changes

--- scripts/client-register.js
+++ scripts/client-register.js
@@ -1,11 +1,38 @@
+const currentUser = {
+    email: '',
+    password: ''
+};
+
 const eyeButton = document.getElementById('toggle-password-btn');
 const passwordInput = document.getElementById('password');
+const eyeIcon = document.querySelector('button img');
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const password = formData.get('user-password');
+    const email = formData.get('user-email');
+
+    console.log(email);
+    if(!email || !password){
+        alert('Preencha todos os campos');
+        return;
+    }
+
+    currentUser.email = email;
+    currentUser.password = password;
+});
 
 eyeButton.addEventListener('click', () => {
     if(passwordInput.type === 'password'){
         passwordInput.type = 'text';
+        eyeIcon.src = '/images/eye-visible.svg';
     }
     else{
         passwordInput.type = 'password';
+        eyeIcon.src = '/images/eye-hidden.svg';
     }
-})
+});
